6df9ccf9858a69a22c5dc74a7a0e97509ffe98d1
Jeremy Lightsmith <jeremy@flave.local>
2010-01-27 16:33:52 -0800

    updated for class 3

--- music/db/schema.rb
+++ music/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100114171046) do
+ActiveRecord::Schema.define(:version => 20100121023353) do
 
   create_table "albums", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,14 @@ ActiveRecord::Schema.define(:version => 20100114171046) do
 
   create_table "artists", :force => true do |t|
     t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tracks", :force => true do |t|
+    t.integer  "number"
+    t.string   "name"
+    t.integer  "album_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
