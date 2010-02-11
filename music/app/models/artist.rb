dd7c4afe77d65901b011479630ef6a23bc60206a
Jeremy Lightsmith <jeremy@d-69-91-167-99.dhcp4.washington.edu>
2010-02-10 18:38:47 -0800

    adding photo

--- music/app/models/artist.rb
+++ music/app/models/artist.rb
@@ -4,4 +4,13 @@ class Artist < ActiveRecord::Base
   def inspect
     "Artist(#{name})"
   end
+  
+  def photo
+    random_photo_to_show_for_now = %w(fear hendrix maxingquaye riding_with_the_king)[rand(4)]
+    Photo.new("/images/album_art/#{random_photo_to_show_for_now}.jpg")
+  end
+  
+  class Photo < Struct.new(:url)
+  end
+  
 end
