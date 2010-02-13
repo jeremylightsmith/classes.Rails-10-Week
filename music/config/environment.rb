f596a4d0e75d832cca301b6b869a107af1bd2f49
Jeremy Lightsmith <jeremy@flave.local>
2010-02-13 13:36:25 -0800

    adding paperclip dependency

--- music/config/environment.rb
+++ music/config/environment.rb
@@ -7,7 +7,7 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # config.gem "paperclip"
+  config.gem "paperclip"
 
   config.time_zone = 'UTC'
 end
